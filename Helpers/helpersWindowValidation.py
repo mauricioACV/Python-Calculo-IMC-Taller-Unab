77ccb939d4285fe2bc50bd9b4ede5e9bcb971bd8
mauricioACV <mauricio.campusano@gmail.com>
2021-11-05 21:46:27 -0300

    mejoramiento de mensajes de validaciones

--- Helpers/helpersWindowValidation.py
+++ Helpers/helpersWindowValidation.py
@@ -4,7 +4,7 @@ def validateFormatCredentials(email, password):
     if(not email and not password):
         return {'response':False, 'message':'Campos vacíos'}
     if(not hf.validEmailFormat(email)):
-        return {'response':False, 'message':'Correo no válido'}
+        return {'response':False, 'message':'Correo no válido, \n el formato requerido es correo@dominio.com'}
     if(not hf.validPassFormat(password)):
         return {'response':False, 'message':'Password no válida! \n intente digitar:\n al menos una letra en mayúscula, , \n al menos una letra en minúscula,\n al menos un carácter especial,\n y al menos un número'}
     return {'response':True}
@@ -17,20 +17,20 @@ def validateDataPerson(name, lastName, gender, age):
     if not hf.onlyString(lastName):
         return {'response':False, 'message':'Apellidos no válidos'}
     if not hf.validAge(age):
-        return {'response':False, 'message':'Edad no válida'}
+        return {'response':False, 'message':'Edad no válida, \n el rango permitodo es entre 15 y 70 años'}
     if not hf.validGender(gender):
-        return {'response':False, 'message':'Género no válido'}
+        return {'response':False, 'message':'Género no válido, \n debe digitar "M" para masculino y "F" para femenino'}
     return {'response':True}
 
 def validateImcData(date, time, weight, height):
     if(not date or not time or not weight or not height):
         return {'response':False, 'message':'Campos vácios'}
     if not hf.validDateFormat(date):
-        return {'response':False, 'message':'Fecha no válida'}
+        return {'response':False, 'message':'Fecha no válida, \n el formato requerido es dd-mm-aaaa'}
     if not hf.validTimeFormat(time):
-        return {'response':False, 'message':'Hora no válida'}
+        return {'response':False, 'message':'Hora no válida, \n el formato requerido es HH:mm'}
     if not hf.onlyIntNumbers(weight):
-        return {'response':False, 'message':'Peso no válido'}
+        return {'response':False, 'message':'Peso no válido, \n el formato requerido es solo número entero en kg.'}
     if not hf.validHeight(height):
-        return {'response':False, 'message':'Altura no válida'}
+        return {'response':False, 'message':'Altura no válida, \n el formato requerido es un número entero y dos decimales ej: 1.90'}
     return {'response':True}
